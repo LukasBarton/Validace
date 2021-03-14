4917920d0e36c720bd7909a20ea7b53344453077
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-03-14 17:45:08 +0100

    ho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {HaDirective} from './ha.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HaDirective
   ],
   imports: [
     BrowserModule,
